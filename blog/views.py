c1689614798cffa1eeda9cfd7018427f12f04f48
Daniel Ochoa <ochoad24@gmail.com>
2019-09-02 12:07:18 -0600

    Se agrego las rutas a blog Se creo una vista Se cargo la data a un template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Publicacion
+from django.utils import timezone
 
 # Create your views here.
+def listar_pub(request):
+    pub=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_creacion')
+    return render(request,'blog/listar_pub.html',{'pub':pub})
